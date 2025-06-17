8b6b94cc309581058bced7875a1510c79229be4d
kkatka <kkatka@mail.ru>
2025-06-17 23:31:17 +0300

    git commit -m git "fix deploy in settings.py"

--- config/settings.py
+++ config/settings.py
@@ -10,8 +10,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = True
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
-
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1").split(",")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
